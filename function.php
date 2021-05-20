14a1578966737dd06c4643a804291a9b08c0225d
cordelia <cordeliasixth@gmail.com>
2021-05-20 12:52:42 +0900

    init

--- function.php
+++ function.php
@@ -1,24 +1,38 @@
 <?php
 
+// ファイルがアップロードされたか確認
 if (is_uploaded_file($_FILES['upload_file']['tmp_name'])) {
+
+  // magic DBをオープンし、MIMEを返すように指定
+  $finfo = finfo_open(FILEINFO_MIME);
+  // アップロードされてきたファイル名を取得
+  $filename = $_FILES["upload_file"]['tmp_name'];
+
+  // 有効な拡張子か確認
+  if (strpos(finfo_file($finfo, $filename), 'text/plain') === false) {
+    // magic DBをクローズ
+    finfo_close($finfo);
+    // エラーメッセージ
+    $error = "不正なファイルです";
+    return $error;
+  }
 
   // ファイルのコンテンツを取得
   $contents = file_get_contents($_FILES['upload_file']['tmp_name']);
-
   // 配列に1文字ずつ格納
   $letters = str_split($contents);
 
-  // asciiの場合、10進数の文字コードに変換する
+  // asciiの場合、10進数の文字コードに変換
   $dec_list;
   foreach ($letters as $letter) {
     $dec_list[] = ord($letter);
   }
 
-  // 2進数に変換する
+  // 2進数に変換
   $bin_list;
   foreach ($dec_list as $dec) {
     $bin_list[] = decbin($dec);
   }
 
-  $toBin = implode($bin_list);
+  $bin_contents = implode($bin_list);
 }
--- index.php
+++ index.php
@@ -9,16 +9,23 @@ require_once('function.php');
   <body>
     <form enctype="multipart/form-data" action="" method="post">
       <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-      <input name="upload_file" type="file" />
-      <p><input type="submit" value="送信" /></p>
+      <input name="upload_file" type="file" /><br>
+      <input type="submit" value="送信" />
+
+      <?php if ($error !== ""): ?>
+        <span><?php echo $error ?></span>
+      <? endif ?>
     </form>
 
+    <p><?php echo $error_message ?></p>
+
     <p>
-      <textarea rows="5" cols="70" placeholder="元のテキストが表示されます""><?php echo $contents ?></textarea>
+      <textarea rows="5" cols="70" placeholder="元のテキストが表示されます"><?php echo $contents ?></textarea>
     </p>
 
     <p>
-      <textarea rows="5" cols="70" placeholder="変換した結果が表示されます"><?php echo $toBin ?></textarea>
+      <textarea rows="5" cols="70" placeholder="変換した結果が表示されます"><?php echo $bin_contents ?></textarea>
     </p>
+
   </body>
 </html>
